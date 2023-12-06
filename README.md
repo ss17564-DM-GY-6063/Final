cb39e4161114247956915b813bfa79369138c62f
shuoshan <ss17564@nyu.edu>
2023-12-06 16:18:53 -0500

    drawheart

--- README.md
+++ README.md
@@ -1,23 +1,49 @@
-# Final Project Template using Serial
+# finalproject
+ 
+## idea 1
+**Music Interaction Project**
+Input from Arduino is used for interactive gestures through infrared sensors. Different distances trigger various sound effects or music, and these diverse musical elements are visualized as beautiful patterns on the p5 canvas.
+One aspect I'm unsure about regarding this idea is how to establish a linkage between Arduino and p5.js.
+![G0PleE.jpg](https://imgpile.com/images/G0PleE.jpg)
 
-Arduino reads a button on pin 2 and a potentiometer on pin A0 and passes those values on to the p5js sketch via a Serial connection.
+## idea 2
+**Physical Color Picker**
+Due to a strong interest in my previous p5.js color filter project, I wanted to create a color picker that allows users to select colors from the physical world and see the results digitally.
+The challenge in this project is how to detect the user's color selection in the physical world, and make connections with digital world. (My initial thought is to use different buttons to represent different colors, but the optimal presentation would involve a physically meaningful color palette.)
+![G0PrCr.jpg](https://imgpile.com/images/G0PrCr.jpg)
 
-The object that is created and sent to p5js looks like this:
+## idea 3
+**Heart Rate NFT**
+By detecting the pulse on the wrist, the frequency of the pulse is transmitted to a computer or another device for visualization, allowing each audience member to receive their own NFT.
+The challenge in this project lies in how to detect the frequency of the pulse and determine the style of visualization.
+[![G0PzVR.jpg](https://imgpile.com/images/G0PzVR.jpg)](https://imgpile.com/i/G0PzVR)
 
-```
-data = {
-  A0: { value: integer },
-  D2: {
-    isPressed: boolean,
-    count: integer
-  }
-}
-```
 
-In p5js ```A0.value``` is used to change ellipse size, ```D2.isPressed``` is used to add new ellipses to an array and ```D2.count``` is used to change the color of new ellipses.
+# project proposal, planning and organizing
+Based on the heart rate sensor, I will get the value of BPM, I’m thinking about showing this BPM in different emojis of different body conditions. In addition, each beat can cause a change to the main pattern.
+![Gg0MWk.png](https://imgpile.com/images/Gg0MWk.png)
 
-## Schematic
-![](https://6063d.github.io/imgs/FinalProject_sch.jpg)
+## System diagram & FSM diagram
+TBD
 
-## Board
-![](https://6063d.github.io/imgs/FinalProject_bb.jpg)
+## Circuit diagram
+![Gg0G3F.jpg](https://imgpile.com/images/Gg0G3F.jpg)
+
+## Description of any external data or library that you are planning to use
+I found some references on https://projecthub.arduino.cc/ that may be related to my project. I currently don't have specific ideas about libraries, and I may not need to use them.
+
+## Description of any sensor, output component or mechanism that you are planning on using or building
+I bought an Ultrasonic Sensor and a Heartbeat Sensor, hoping that Amazon can deliver them on time. In case the modules prove to be challenging to work with, I will start by experimenting with my p5.js effects. Additionally, I have planned to design the appearance of the wearable device to enhance its user-friendliness in interaction.
+
+## Reference images, texts and projects
+some reference to the sensor tutorial: 
+https://projecthub.arduino.cc/Isaac100/getting-started-with-the-hc-sr04-ultrasonic-sensor-7cabe1
+https://projecthub.arduino.cc/SurtrTech/measure-heart-rate-and-spo2-with-max30102-eb4f74
+https://www.youtube.com/watch?v=z4ztmNpBmZk
+https://pulsesensor.com/pages/code-and-guide
+
+## Plan for user testing
+To ensure that each heartbeat project is unique, I will conduct a user test shortly after achieving the basic functionality. I will then make adjustments based on the results of the user test.
+
+## Short discussion of why your project is relevant:
+I hope to use the fundamental coding knowledge we've acquired to assist more people, incorporating not only artistic elements but also practical applications.
--- sketch.js
+++ sketch.js
@@ -1,84 +1,136 @@
-// serial variables
-let mSerial;
+// // serial variables
+// let mSerial;
 
-let connectButton;
+// let connectButton;
 
-let readyToReceive;
+// let readyToReceive;
 
-// project variables
-let mElls = [];
+// // project variables
+// let mElls = [];
 
-function receiveSerial() {
-  let line = mSerial.readUntil("\n");
-  trim(line);
-  if (!line) return;
+// function receiveSerial() {
+//   let line = mSerial.readUntil("\n");
+//   trim(line);
+//   if (!line) return;
 
-  if (line.charAt(0) != "{") {
-    print("error: ", line);
-    readyToReceive = true;
-    return;
-  }
+//   if (line.charAt(0) != "{") {
+//     print("error: ", line);
+//     readyToReceive = true;
+//     return;
+//   }
 
-  // get data from Serial string
-  let data = JSON.parse(line).data;
-  let a0 = data.A0;
-  let d2 = data.D2;
+//   // get data from Serial string
+//   let data = JSON.parse(line).data;
+//   let a0 = data.A0;
+//   let d2 = data.D2;
 
-  // use data to update project variables
-  if (d2.isPressed) {
-    mElls.push({
-      x: random(width),
-      y: random(height),
-      c: map(d2.count % 20, 0, 20, 155, 255),
-      d: map(a0.value, 0, 4095, 20, 200),
-    });
-  }
+//   // use data to update project variables
+//   if (d2.isPressed) {
+//     mElls.push({
+//       x: random(width),
+//       y: random(height),
+//       c: map(d2.count % 20, 0, 20, 155, 255),
+//       d: map(a0.value, 0, 4095, 20, 200),
+//     });
+//   }
 
-  // serial update
-  readyToReceive = true;
-}
+//   // serial update
+//   readyToReceive = true;
+// }
 
-function connectToSerial() {
-  if (!mSerial.opened()) {
-    mSerial.open(9600);
+// function connectToSerial() {
+//   if (!mSerial.opened()) {
+//     mSerial.open(9600);
 
-    readyToReceive = true;
-    connectButton.hide();
-  }
-}
+//     readyToReceive = true;
+//     connectButton.hide();
+//   }
+// }
+
+// function setup() {
+//   // setup project
+//   createCanvas(windowWidth, windowHeight);
+
+//   // setup serial
+//   readyToReceive = false;
+
+//   mSerial = createSerial();
+
+//   connectButton = createButton("Connect To Serial");
+//   connectButton.position(width / 2, height / 2);
+//   connectButton.mousePressed(connectToSerial);
+// }
+
+// function draw() {
+//   // project logic
+//   background(0);
+//   for (let i = 0; i < mElls.length; i++) {
+//     let me = mElls[i];
+//     fill(me.c, 0, 0);
+//     ellipse(me.x, me.y, me.d, me.d);
+//   }
+
+//   // update serial: request new data
+//   if (mSerial.opened() && readyToReceive) {
+//     readyToReceive = false;
+//     mSerial.clear();
+//     mSerial.write(0xab);
+//   }
+
+//   // update serial: read new data
+//   if (mSerial.availableBytes() > 8) {
+//     receiveSerial();
+//   }
+// }
+
+
 
 function setup() {
-  // setup project
-  createCanvas(windowWidth, windowHeight);
+	createCanvas(400, 400);
+	background(0);
+}
 
-  // setup serial
-  readyToReceive = false;
-
-  mSerial = createSerial();
-
-  connectButton = createButton("Connect To Serial");
-  connectButton.position(width / 2, height / 2);
-  connectButton.mousePressed(connectToSerial);
+function drawheart() {
+  drawingContext.shadowOffsetX = 0;
+  drawingContext.shadowOffsetY = 0;
+  drawingContext.shadowBlur = 10;
+  drawingContext.shadowColor = '#FFFFFF';
+	fill(255,255,0);
+	strokeWeight(2);
+	stroke(255,255,255,100);
+	push();
+	beginShape();
+	
+	for(var a = 0; a < TWO_PI; a += 0.01){
+		var r = noise(10,10,frameCount/50)*10   //控制爱心变大小的重要参数
+		var x = r * 16 * pow(sin(a),3);
+		var y = -r * (13*cos(a) - 5*cos(2*a) - 2*cos(3 * a) - cos(4 * a));
+		vertex(x,y);
+	}
+	endShape();
+	pop();
 }
 
 function draw() {
-  // project logic
-  background(0);
-  for (let i = 0; i < mElls.length; i++) {
-    let me = mElls[i];
-    fill(me.c, 0, 0);
-    ellipse(me.x, me.y, me.d, me.d);
-  }
-
-  // update serial: request new data
-  if (mSerial.opened() && readyToReceive) {
-    readyToReceive = false;
-    mSerial.clear();
-    mSerial.write(0xab);
-  }
-
-  // update serial: read new data
-  if (mSerial.availableBytes() > 8) {
-    receiveSerial();
-  }
+	background(0,10);
+	translate(width/2,height/2);
+	drawheart();
+  // drawingContext.shadowOffsetX = 0;
+  // drawingContext.shadowOffsetY = 0;
+  // drawingContext.shadowBlur = 10;
+  // drawingContext.shadowColor = '#FFFFFF';
+	// fill(255,255,0);
+	// strokeWeight(2);
+	// stroke(255,255,255,100);
+	// push();
+	// beginShape();
+	
+	// for(var a = 0; a < TWO_PI; a += 0.01){
+	// 	var r = noise(10,10,frameCount/50)*10   //控制爱心变大小的重要参数
+	// 	var x = r * 16 * pow(sin(a),3);
+	// 	var y = -r * (13*cos(a) - 5*cos(2*a) - 2*cos(3 * a) - cos(4 * a));
+	// 	vertex(x,y);
+	// }
+	// endShape();
+	// pop();
 }
